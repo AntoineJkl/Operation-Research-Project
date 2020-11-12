918a5c21770432d31d6f2b2e5620cae3a3504f4b
victorius35 <victor.bertret@gmail.com>
2020-11-12 17:21:38 +0100

    Update ArbreEnumeration.py

--- ArbreEnumeration.py
+++ ArbreEnumeration.py
@@ -6,23 +6,23 @@ class ArbreEnumeration:
     
     def __init__(self,d1,p1):
             self.nombre_noeuds=1
-            self.tree=[Noeud("Problème Initial",())]
+            self.tree=[Noeud("Problème Initial",[0],"No")]
             self.dual_bounds=[d1]
             self.primal_bounds=[p1]
     
-    def ajouterBranchement(self,noeud1,noeud2):
+    def ajouterBranchement(self,descript1,descript2,pos):
         self.nombre_noeuds+=2
-        self.tree= self.tree+[noeud1,noeud2]
+        self.tree= self.tree+[Noeud(descript1,pos+[1],"No"),Noeud(descript2,pos+[2],"No")]
 
 class Noeud:
     
-    def __init__(self,description,position):
+    def __init__(self,description,position,state):
         self.description=description
-        self.position=position #tupple pour définir la position donnant les directions qu'il faut emprunter du début de l'arbre jusqu'à la fin
-    
+        self.position=position #list pour définir la position donnant les directions qu'il faut emprunter du début de l'arbre jusqu'à la fin
+        self.state=state
     
 if __name__ == "__main__":
     a = ArbreEnumeration(1000,0)
     print(a.tree[0].position)
-    a.ajouterBranchement(Noeud("1",(1)),Noeud("2",(2)))
-    print(a.tree[1].position)
+    a.ajouterBranchement("1","2",[0])
+    print(a.tree[2].position)
